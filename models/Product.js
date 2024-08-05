763a8fc4dbefc48a56e3033b141ca422af8e0316
AllredW <allrewil@gmail.com>
2024-08-05 19:14:41 -0400

    confirmed get route works for categories, getting blank return for products/tags

--- models/Product.js
+++ models/Product.js
@@ -17,15 +17,26 @@ Product.init(
     },
     product_name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
       type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       references: {
         model: 'category',
         key: 'id',
